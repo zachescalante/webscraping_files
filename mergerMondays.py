8e375fd61807ffbd4440f2a0519ccc0bd3f6b735
zachescalante <zach.escalante@gmail.com>
2020-02-04 15:31:08 -0500

    Test parsing file

--- mergerMondays.py
+++ mergerMondays.py
@@ -1,1 +1,19 @@
 import re
+
+import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
+
+webpage = 'https://www.insidearbitrage.com/2020/02/merger-arbitrage-mondays-comtech-telecommunications-acquires-gilat-satellite-networks-in-a-cash-plus-stock-deal/'
+
+with urllib.request.urlopen(webpage) as response:
+   html = response.read()
+
+soup = BeautifulSoup(html, features='lxml')
+
+deals = soup.find_all('ol')[0].find_all('li')
+
+for deal in deals:
+    print(deal.text)
+    [print(tag['href'], tag.text) for tag in deal.find_all('a', href=True, text=True)]
+
